f0deadfc371f19f1782bbf35a57b5ba0b7896d70
imolinap9191 <imolinap91@gmail.com>
2023-03-06 21:27:41 -0300

    actualizacion

--- Ejercicio Obligatorio/Auto/autos.ts
+++ Ejercicio Obligatorio/Auto/autos.ts
@@ -1,3 +1,4 @@
+// Definir la clase Auto con sus variables internas y constructor 
 export class Auto{
 public brand:string;
 public model:string;
@@ -16,6 +17,8 @@ constructor(brand:string,model:string,years:number,colour:string,origin:string,o
     this.patent=patent;
 }
 }
+
+// Implemente la clase RegistroAutomotor  que soporte funciones insertar/consultar/modificar/eliminar autos 
 export class RegistroAutomotor{
     registroAutomotor(buscador:any){
         console.log('Autos: ', buscador);
@@ -67,22 +70,40 @@ export class RegistroAutomotor{
     }
 }
 
+
+//Crear varios instancias de autos para simular registro automotor
 const newCar =new Auto('Ford','Ka',2012,'Negro','Brasil','juanGarcia','LYO619');
 const newCarTwo =new Auto('Chevrolet','Cruze',2014,'Blanco','EEUU','juanPerez','MNH807');
 const newCarThree =new Auto('Toyota','Hilux 4x4',2015,'Blanca','Japon','juanLopez','OKO817');
 const newCarFour =new Auto('Chevrolet','Celta',2012,'Rojo','Brasil','juanGonzalez','KWO986');
 const newCarFive =new Auto('Toyota','Hilux 4x4',2013,'Arena','Japon','juanRodriguez','NVZ547');
 const newCarsix =new Auto('Chevrolet','Onix',2016,'Gris','Argentina','juanVelez','AA437ON');
-
+//Poblar el listado con autos
 const listado= [newCar,newCarTwo,newCarThree,newCarFour,newCarFive,newCarsix]
+//Crear nuevo registro de autos con las instancias creadas
 const registro= new RegistroAutomotor
+//cree una nueva instancia para agregar
 const newCarSeven = new Auto('Ford', 'Focus',2021,'Gris','Brasil','juanFlores','AF315AA')
-//registro.insertar(newCarSeven,listado)
-//registro.consultar('LYO619',listado)
-//registro.eliminar('LYO619',listado)
-//registro.modificar('LYO619',listado, 'LYO620')
-//registro.registroAutomotor(listado)
+//funcion ejecutar en marcha
+registro.insertar(newCarSeven,listado)
+//funcion consultar en marcha
+registro.consultar('LYO619',listado)
+//funcion eliminar en marcha
+registro.eliminar('LYO619',listado)
+//funcion modificar en marcha
+registro.modificar('LYO619',listado, 'LYO620')
+//funcion de la totalidad en marcha
+registro.registroAutomotor(listado)
 
+//utilizar fs para incorporar datos desde un archivo JSON
 import * as fs from "fs"
+const carro:string[]=[]
 const base=fs.readFileSync("./autos.json", "utf8");
+const carros=JSON.parse(base)
+for(let i=0; i<carros.length;i++){
+    carro.push(carros[i])
+}
+//ver que sucede con una de las funciones creadas posterior a la incorporacion de los datos provenientes de JSON
+registro.insertar(carros,listado)
+
 
