ecbdbc1b0c33a9360c2b5ad0885964c51369916e
imolinap9191 <imolinap91@gmail.com>
2023-03-25 20:59:37 -0300

    actualizacion

--- EjercicioObligatorio230323/index.ts
+++ EjercicioObligatorio230323/index.ts
@@ -3,10 +3,18 @@ import GestorClub from "./ClaseGestorClub";
 let independiente =new GestorClub()
 independiente.agregarSocio()
 independiente.agregarSocio()
-//independiente.buscarPorApellido("molina")
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.agregarSocio()
+independiente.buscarPorApellido("molina")
 independiente.buscarPorContacto("2281325015")
 independiente.buscarPorDNI("36362001")
-//independiente.buscarPorNombre("pedro")
+independiente.buscarPorNombre("pedro")
 
 
 
